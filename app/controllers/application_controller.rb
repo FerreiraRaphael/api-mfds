b3bc5c4f4ac976794b25c58d1045b6f241f13c09
Raphael Ferreira <raphaelbsferreira@gmail.com>
2016-11-09 18:32:16 -0200

    metodos para as respostas da api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  def resource_dont_exist
+    {
+      objeto: [],
+      messagem: 'nao existe'
+    }
+  end
+
+  def resource_exist(resource)
+    objeto = resource.errors.empty? ? resource : resource.errors
+    { objeto: objeto, messagem: 'sucesso' }
+  end
+
+  def json_response(resource)
+    resource.nil? ? resource_dont_exist : resource_exist(resource)
+  end
 end
