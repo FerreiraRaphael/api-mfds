8b4233c20220cb9178f8efed5976e7df1462e8ab
Raphael Ferreira <raphaelbsferreira@gmail.com>
2016-11-09 18:33:21 -0200

    resource routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :resources, only: [:show, :create, :index, :update, :destroy]
+
+  root 'resources#index'
 end
