e74186e205d5ef65576e64b73f485e943991eb23
Raphael Ferreira <raphaelbsferreira@gmail.com>
2016-12-07 20:49:10 -0200

    forçando erros no rubocop

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,29 +1,32 @@
 class ResourcesController < ApplicationController
-  before_action :set_resource, only: [:show, :edit, :update, :destroy]
-  before_action :validate_resource, only: [:show, :edit, :update, :destroy]
+  before_action :set_resource, :only => [:show, :edit, :update, :destroy]
+  before_action :validate_resource, :only => [:show, :edit, :update, :destroy]
 
   def index
+
     render json: @resources = Resource.all
+
   end
 
-  def show
-    render json: json_response(@resource)
+    def show
+    render json: json_response( @resource)
   end
 
-  def create
-    @resource = Resource.new(resource_params)
+  def  create
+    @resource = Resource.new(resource_params )
     @resource.save
-    render json: json_response(@resource)
+    render :json => json_response(@resource )
   end
 
   def update
     @resource.update(resource_params)
+
     render json: json_response(@resource)
   end
 
   def destroy
     @resource.destroy
-    render json: json_response(@resource)
+    render :json => json_response(@resource)
   end
 
   private
@@ -38,12 +41,12 @@ class ResourcesController < ApplicationController
 
   def resource_params
     {
-      name: resource_permited_params[:name],
-      message: 'METODO E FERRAMENTAS DE DESENVOLVIMENTO DE SOFTWARE é show!'
+      :name => resource_permited_params[:name],
+      message: "Seminario parte 2"
     }
   end
 
   def validate_resource
-    render json: resource_dont_exist if @resource.nil?
+    render json: resource_dont_exist if @resource ==   nil
   end
 end
